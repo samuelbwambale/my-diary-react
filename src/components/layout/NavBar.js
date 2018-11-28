b3e502c3149ac5dca53137a36918eebe7cceef6b
Samuel Bwambale <31615608+samuelbwambale@users.noreply.github.com>
2018-11-28 17:34:05 +0300

    chore(redux): setup up redux router
    - Add redux and create redux file structure
    
    [Finishes #162193221]

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const NavBar = () => (
   <React.Fragment>
     <nav className="navbar fixed navbar-expand-lg nav-bg">
-        <div className="">
-          <Link className="nav-bar-text" exact to="/">My Diary </Link>
-        </div>
-        <div className="collapse navbar-collapse float-right" id="navbarSupportedContent">
-          <Link className="nav-link active float-right" exact to="signup">Signup</Link>
-          <Link className="nav-link active float-right" exact to="login">Login</Link>
-        </div>
+      <div className="">
+        <Link className="nav-bar-text" exact to="/">My Diary </Link>
+      </div>
+      <div className="collapse navbar-collapse float-right" id="navbarSupportedContent">
+        <Link className="nav-link active float-right" exact to="signup">Signup</Link>
+        <Link className="nav-link active float-right" exact to="login">Login</Link>
+      </div>
     </nav>
   </React.Fragment>
 );
